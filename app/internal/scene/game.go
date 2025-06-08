27b629854ed81574547387431f3fe0955cbc1db3
merki86 <nassen.d230@gmail.com>
2025-06-08 20:35:20 +0500

    garbage clean

--- app/internal/scene/game.go
+++ app/internal/scene/game.go
@@ -35,13 +35,11 @@ func (s *gameScene) Update(dt time.Duration) {
 
 func (s *gameScene) Draw(sc tcell.Screen) {
 	s.game.Mutex.Lock()
-	for y := 1; y < 3; y += 10 {
-		for i := 1; i < 5; i++ {
-			print.Print(sc, textPos-i-y, i, fmt.Sprintf("%v.upper.%v", textPos, textPos))
-		}
-		for i := 1; i < 5; i++ {
-			print.Print(sc, textPos+i-5-y, i+4, fmt.Sprintf("%v.bottom.%v", textPos, textPos))
-		}
+	for i := 1; i < 5; i++ {
+		print.Print(sc, textPos-i, i, fmt.Sprintf("%v.upper.%v", textPos, textPos))
+	}
+	for i := 1; i < 5; i++ {
+		print.Print(sc, textPos+i-5, i+4, fmt.Sprintf("%v.bottom.%v", textPos, textPos))
 	}
 	s.game.Mutex.Unlock()
 }
